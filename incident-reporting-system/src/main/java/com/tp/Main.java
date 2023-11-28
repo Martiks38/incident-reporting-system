ed034d0c7f79d232b4aaa369c9524bc636b9dbbb
Martiks <cappa.giuliano@gmail.com>
2023-11-28 19:54:34 -0300

    remove check constraint foreign key

--- incident-reporting-system/src/main/java/com/tp/Main.java
+++ incident-reporting-system/src/main/java/com/tp/Main.java
@@ -1,9 +1,26 @@
 package com.tp;
 
+import com.tp.application.GetEntityManager;
+
+import jakarta.persistence.EntityManager;
+// import jakarta.persistence.EntityTransaction;
+
 public class Main {
 
     public static void main(String[] args) {
+        EntityManager manager = GetEntityManager.getManager();
         
+        // Funcionar funciona me molesta el ruido en consola
+
+        // EntityTransaction tx = manager.getTransaction();
+
+        // tx.begin();
+
+        // name = user.getEmail();
+        // User user = new User();
+
+        // em.persist(u);
+        // tx.commit();
+        System.out.println("null");
     }
-    
 }
